9e1b1b26bd98ab576953f01caa1ce0b2795ebad3
sblauens <sblauens@student.42.fr>
2018-04-30 18:08:14 +0200

    Make dotfiles appearence an option
    By default all files were listed, including dotfiles. Now, entries
    begining with a dot are ignored by default and unignored if specified by
    option.

--- src/read.c
+++ src/read.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 15:09:13 by sblauens          #+#    #+#             */
-/*   Updated: 2018/04/30 03:32:02 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/04/30 17:16:51 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,17 +54,21 @@ int					get_dir_content(char *dir_name, t_list **dir_files,
 		error_exit();
 	while ((dir_entry = readdir(dir_stream)))
 	{
-		ft_strcpy(file.pathname, dir_name);
-		ft_strcat(file.pathname, "/");
-		ft_strcat(file.pathname, dir_entry->d_name);
-		ft_strcpy(file.filename, dir_entry->d_name);
-		file.subfiles = NULL;
-		if (!(node = ft_lstnew(&file, sizeof(file))))
-			return (-1);
-		if (!*dir_files)
-			*dir_files = node;
-		else
-			ft_lstadd_bck(dir_files, node);
+		if (((options->dotfiles == none) && (*(dir_entry->d_name) != '.'))
+				|| (options->dotfiles == all))
+		{
+			ft_strcpy(file.pathname, dir_name);
+			ft_strcat(file.pathname, "/");
+			ft_strcat(file.pathname, dir_entry->d_name);
+			ft_strcpy(file.filename, dir_entry->d_name);
+			file.subfiles = NULL;
+			if (!(node = ft_lstnew(&file, sizeof(file))))
+				return (-1);
+			if (!*dir_files)
+				*dir_files = node;
+			else
+				ft_lstadd_bck(dir_files, node);
+		}
 	}
 	print_dir_content(*dir_files);
 	if (options->recursive)
