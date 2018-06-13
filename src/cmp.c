626aafb5a236c3c927f30f4ca176861639e4f831
sblauens <sblauens@student.42.fr>
2018-06-13 03:33:15 +0200

    Add reverse sorting
    Reverse order for any sort rule.

--- src/cmp.c
+++ src/cmp.c
@@ -6,18 +6,26 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 13:53:52 by sblauens          #+#    #+#             */
-/*   Updated: 2018/05/31 23:21:35 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/06/13 03:22:30 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int			cmp_filename(void *f1, void *f2)
+extern t_options				g_options;
+
+int								cmp_filename(void *f1, void *f2)
 {
-	return (ft_strcmp(((t_file *)f1)->filename, ((t_file *)f2)->filename));
+	if (g_options.reverse)
+		return (ft_strcmp(((t_file *)f2)->filename, ((t_file *)f1)->filename));
+	else
+		return (ft_strcmp(((t_file *)f1)->filename, ((t_file *)f2)->filename));
 }
 
-int			cmp_dirname(void *d1, void *d2)
+int								cmp_dirname(void *d1, void *d2)
 {
-	return (ft_strcmp((char *)d1, (char *)d2));
+	if (g_options.reverse)
+		return (ft_strcmp((char *)d2, (char *)d1));
+	else
+		return (ft_strcmp((char *)d1, (char *)d2));
 }
