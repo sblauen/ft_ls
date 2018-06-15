43e3ea349a05318eda1acbc96a4375c512f3c6b2
sblauens <sblauens@student.42.fr>
2018-06-15 03:03:42 +0200

    Refactor comparison functions
    Comparison was specific to each option. This has been changed to be more
    generic, options are now applied if needed and then the comparison is
    made.

--- src/cmp.c
+++ src/cmp.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 13:53:52 by sblauens          #+#    #+#             */
-/*   Updated: 2018/06/13 03:22:30 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/06/15 02:56:47 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@ extern t_options				g_options;
 int								cmp_filename(void *f1, void *f2)
 {
 	if (g_options.reverse)
-		return (ft_strcmp(((t_file *)f2)->filename, ((t_file *)f1)->filename));
-	else
-		return (ft_strcmp(((t_file *)f1)->filename, ((t_file *)f2)->filename));
+		ft_swapptr(&f1, &f2);
+	return (ft_strcmp(((t_file *)f1)->filename, ((t_file *)f2)->filename));
 }
 
 int								cmp_dirname(void *d1, void *d2)
 {
 	if (g_options.reverse)
-		return (ft_strcmp((char *)d2, (char *)d1));
-	else
-		return (ft_strcmp((char *)d1, (char *)d2));
+		ft_swapptr(&d1, &d2);
+	return (ft_strcmp((char *)d1, (char *)d2));
 }
