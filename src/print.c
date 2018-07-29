72249ecf6310ef8f98a4585a48c530e147758abf
sblauens <sblauens@student.42.fr>
2018-07-29 17:18:00 +0200

    Fix right alignement when writing nlinks
    Long listing output does not align the numbers of hard links to the
    right. This results in a misaligned output of a directory's content.
    
    Adding the needed number of spaces on the left of each nlink solves this
    issue.

--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 18:58:21 by sblauens          #+#    #+#             */
-/*   Updated: 2018/07/28 20:40:17 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/07/29 17:57:40 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,30 @@
 #include <uuid/uuid.h>
 #include "ft_ls.h"
 
-static inline void		longlist_link_uid_gid(t_file *file)
+static inline size_t	get_spaces(t_list *files)
+{
+	size_t				spaces_nlink;
+	size_t				ret_nlink;
+
+	spaces_nlink = 0;
+	while (files)
+	{
+		ret_nlink = ft_nbrpwr(((t_file *)(files->content))->st_nlink);
+		if (ret_nlink > spaces_nlink)
+			spaces_nlink = ret_nlink;
+		files = files->next;
+	}
+	return (spaces_nlink);
+}
+
+static inline void		longlist_link_uid_gid(t_file *file, size_t spaces_nlink)
 {
 	struct passwd		*pw;
 	struct group		*gr;
 
 	pw = getpwuid(file->st_uid);
 	gr = getgrgid(file->st_gid);
-	ft_putchar(' ');
-	ft_putnbr(file->st_nlink);
+	ft_putnbr_ralign(file->st_nlink, spaces_nlink);
 	ft_putchar(' ');
 	ft_putstr(pw->pw_name);
 	ft_putstr("  ");
@@ -31,20 +46,22 @@ static inline void		longlist_link_uid_gid(t_file *file)
 	ft_putchar(' ');
 }
 
-static inline void	print_longlist(t_file *file)
+static inline void		print_longlist(t_file *file, size_t spaces_nlink)
 {
-	char			buf[12];
+	char				buf[13];
 
 	longlist_modes(file, buf);
 	buf[10] = ' ';
-	buf[11] = 0;
+	buf[11] = ' ';
+	buf[12] = 0;
 	ft_putstr(buf);
-	longlist_link_uid_gid(file);
+	longlist_link_uid_gid(file, spaces_nlink);
 }
 
-void				print_dir(char *dir_name, t_list *dir_files)
+void					print_dir(char *dir_name, t_list *dir_files)
 {
-	t_list		*tmp;
+	t_list				*tmp;
+	size_t				spaces_nlink;
 
 	tmp = dir_files;
 	if (g_options.multi_files)
@@ -52,10 +69,11 @@ void				print_dir(char *dir_name, t_list *dir_files)
 		ft_putstr(dir_name);
 		ft_putstr(":\n");
 	}
+	spaces_nlink = get_spaces(tmp);
 	while (tmp)
 	{
 		if (g_options.format == long_listing)
-			print_longlist(((t_file *)(tmp->content)));
+			print_longlist(((t_file *)(tmp->content)), spaces_nlink);
 		ft_putendl(((t_file *)(tmp->content))->filename);
 		tmp = tmp->next;
 	}
