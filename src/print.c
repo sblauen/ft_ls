ba8eabebf0f269956da79a4822662e16462c6154
sblauens <sblauens@student.42.fr>
2018-08-07 00:35:36 +0200

    print_dir: remove unsused argument

--- src/print.c
+++ src/print.c
@@ -6,18 +6,17 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 18:58:21 by sblauens          #+#    #+#             */
-/*   Updated: 2018/08/06 17:00:02 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/08/06 21:42:10 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void					print_dir(char *dir_name, t_list *dir_files)
+void					print_dir(t_list *dir_files)
 {
 	t_list				*tmp;
 
 	tmp = dir_files;
-	dir_name = NULL;
 	if (g_options.format == long_listing)
 	{
 		longlist_print(dir_files);
