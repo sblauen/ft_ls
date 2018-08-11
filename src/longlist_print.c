df9422087d27bcf9c9f87a60c144e2e0919f2cde
sblauens <sblauens@student.42.fr>
2018-08-11 20:59:41 +0200

    Reduce long list buffer allocations
    For the long list output, each printed line is an unique allocated
    string, depending on the filname's length. Consequently, malloc(3) and
    free(3) have to be called for each line.
    
    This can be avoided using each directory's longest contained filename.

--- src/longlist_print.c
+++ src/longlist_print.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 01:49:51 by sblauens          #+#    #+#             */
-/*   Updated: 2018/08/11 16:34:25 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/08/11 20:10:41 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,10 @@ static inline size_t		longlist_id(t_file *file, t_sizes *sp, char *buf)
 	return (len);
 }
 
-char					*longlist_buf(t_file *file, t_sizes *sp)
+void					longlist_buf(t_file *file, t_sizes *sp, char *buf)
 {
-	char				*buf;
 	size_t				len;
 
-	if (!(buf = ft_strnew(sp->len + ft_strlen(file->filename))))
-		return (NULL);
 	longlist_modes(file, buf);
 	len = nbr_align(buf + 12, file->st_nlink, sp->nlink) + 12;
 	len += longlist_id(file, sp, buf + len);
@@ -61,5 +58,4 @@ char					*longlist_buf(t_file *file, t_sizes *sp)
 	len += ft_strlcpy(buf + len, ctime(&(file->mtime.tv_sec)) + 3, 14) - 9;
 	len += ft_strlcpy(buf + len, " ", 2);
 	ft_strcpy(buf + len, file->filename);
-	return (buf);
 }
