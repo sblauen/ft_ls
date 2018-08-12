79ba50c1a64a91f397bca90bb70c3003133003b9
sblauens <sblauens@student.42.fr>
2018-08-12 18:58:37 +0200

    Remove unused variable in nbr_align()

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: sblauens <sblauens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 16:00:56 by sblauens          #+#    #+#             */
-/*   Updated: 2018/08/12 15:31:52 by sblauens         ###   ########.fr       */
+/*   Updated: 2018/08/12 18:30:24 by sblauens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 size_t					nbr_align(char *buf, int n, size_t sp)
 {
 	register size_t		len;
-	register int		k;
 
-	k = 10;
 	buf[sp] = 0;
 	len = sp;
 	if (!n)
